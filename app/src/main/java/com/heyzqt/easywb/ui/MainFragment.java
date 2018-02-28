0b58a243f56106bfc7454fa6a68ede666ff8620e
heyzqt <heyzqt@qq.com>
2018-03-01 01:23:37 +0800

    在MainFragment中添加ScrollView，解决listview的显示不全问题，和listveiw与ScrollView的滑动冲突问题

--- app/src/main/java/com/heyzqt/easywb/ui/MainFragment.java
+++ app/src/main/java/com/heyzqt/easywb/ui/MainFragment.java
@@ -1,6 +1,7 @@
 package com.heyzqt.easywb.ui;
 
 
+import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,14 +9,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.heyzqt.easywb.R;
 import com.heyzqt.easywb.data.model.Temp;
 import com.heyzqt.easywb.ui.adapter.BaseAbsListAdapter;
 import com.heyzqt.easywb.ui.adapter.BaseViewHolder;
+import com.heyzqt.easywb.widget.ExpandListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,11 +27,13 @@ import java.util.List;
  * Created by heyzqt on 2018/2/26.
  */
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements AdapterView.OnItemClickListener {
 
 	public static MainFragment mMainFragment = null;
 
-	private ListView mListView;
+	private ExpandListView mListView;
+
+	private Context mContext;
 
 	public static MainFragment newInstance() {
 		if (mMainFragment == null) {
@@ -42,12 +47,15 @@ public class MainFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
 			@Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_main, container, false);
+		mContext = view.getContext();
 		initView(view);
 		return view;
 	}
 
 	private void initView(View view) {
 		mListView = view.findViewById(R.id.listview_main_content);
+		mListView.setFocusable(false);
+		mListView.setFocusableInTouchMode(false);
 
 		List<Temp> datas = new ArrayList();
 		Temp temp = new Temp();
@@ -71,7 +79,7 @@ public class MainFragment extends Fragment {
 		temp.content = "自动参考线在Sketch的默认设置中是被打"
 				+ "开的，你可以同时按住 control 键和 L 键"
 				+ "来关闭它。当你在调节一个图层的大小或"
-				+ "者移动一个图层的位置时，Sketch会自动";
+				+ "者移动一个图层的位置时，Sketch会自动。";
 		datas.add(temp);
 		temp = new Temp();
 		temp.avatar = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_round);
@@ -80,7 +88,7 @@ public class MainFragment extends Fragment {
 		temp.content = "自动参考线在Sketch的默认设置中是被打"
 				+ "开的，你可以同时按住 control 键和 L 键"
 				+ "来关闭它。当你在调节一个图层的大小或"
-				+ "者移动一个图层的位置时，Sketch会自动";
+				+ "者移动一个图层的位置时，Sketch会自动。";
 		datas.add(temp);
 		mListView.setAdapter(new BaseAbsListAdapter<Temp>(view.getContext(), datas,
 				R.layout.item_main_listview_content) {
@@ -96,5 +104,12 @@ public class MainFragment extends Fragment {
 				content.setText(item.content);
 			}
 		});
+
+		mListView.setOnItemClickListener(this);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		Toast.makeText(mContext, "item = " + position, Toast.LENGTH_SHORT).show();
 	}
 }
